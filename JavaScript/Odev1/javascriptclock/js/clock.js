3b8c06e75055ec42b7a0aa748da2857f0513c791
Ayşe Karataş <aysecratas@gmail.com>
2021-06-09 01:48:43 +0300

    Update clock.js

--- JavaScript/Odev1/javascriptclock/js/clock.js
+++ JavaScript/Odev1/javascriptclock/js/clock.js
@@ -1,12 +1,12 @@
-let hrElement = document.querySelector(".hr")
-let minElement = document.querySelector(".min")
+let hrElement = document.querySelector(".hr");
+let minElement = document.querySelector(".min");
 let secElement = document.querySelector(".sec");
-let nameElement = document.querySelector("#myName")
+let nameElement = document.querySelector("#myName");
 let dayElement = document.querySelector(".day");
-const days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"]
+const days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
 
 let hr, min, sec, day, name;
-name = window.prompt("Adınız nedir? ")
+name = window.prompt("Adınız nedir? ");
 nameElement.innerHTML = name;
 
 setInterval(function(){
@@ -20,4 +20,4 @@ setInterval(function(){
     secElement.innerHTML = sec<10 ? `0${sec}` : sec;
     dayElement.innerHTML = days[day];
 
-},1000)
+},1000);
